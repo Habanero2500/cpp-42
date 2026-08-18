602235dce6728931facfd50b955bcf180b292cb5
jmetayer <julien.metayer98@gmail.com>
2026-08-18 18:12:20 +0200

    Add Bureaucrat.cpp

--- cpp05/ex00/Bureaucrat.cpp
+++ cpp05/ex00/Bureaucrat.cpp
@@ -6,19 +6,60 @@
 /*   By: jmetayer <jmetayer@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/18 17:45:59 by jmetayer          #+#    #+#             */
-/*   Updated: 2026/08/18 17:51:07 by jmetayer         ###   ########.fr       */
+/*   Updated: 2026/08/18 18:11:49 by jmetayer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Bureaucrat.hpp"
 
-    Bureaucrat ( void );
-    Bureaucrat ( std::string name );
-    Bureaucrat& operator=( const Bureaucrat& copy );
-    Bureaucrat ( const Bureaucrat &copy );
-    ~Bureaucrat();
+Bureaucrat::Bureaucrat ( void )
+{
+    std::cout << "Bureaucrat default constructor is used" << std::endl;
+}
 
-    void setName( std::string name );
-    std::string getName( void );
-    void setRange( int range );
-    int getRange ( void );
+Bureaucrat::Bureaucrat ( std::string name )
+{
+    std::cout << "Bureaucrat classical constructor is used" << std::endl; 
+    _name = name; 
+}
+
+Bureaucrat& Bureaucrat::operator=( const Bureaucrat& copy )
+{
+    std::cout << "Bureaucrat operator overload = is called" << std::endl; 
+    if(*this != copy)
+    {
+        _name = copy.getName();
+        _range = copy.getRange();
+    }
+    return(*this);   
+}
+
+Bureaucrat::Bureaucrat ( const Bureaucrat &copy )
+{
+    std::cout <<"Bureaucrat copy constructor called" << std::endl;
+    _name = copy.getName();
+    _range = copy.getRange();
+}
+
+Bureaucrat::~Bureaucrat()
+{
+    std::cout << "Bureaucrat destructor is called" << std::endl; 
+}
+
+void Bureaucrat::setName( std::string name )
+{
+    _name = name;
+}
+std::string Bureaucrat::getName( void )
+{
+    return (this->_name);
+}
+void Bureaucrat::setRange( int range )
+{
+    _range = range;
+}
+    
+int Bureaucrat::getRange ( void )
+{
+    return (_range);
+}
