f9587a378700ddbb0b75654afb62c96740a2605b
jmetayer <julien.metayer98@gmail.com>
2026-08-18 17:51:25 +0200

    Commit ex00

--- cpp05/ex00/Bureaucrat.cpp
+++ cpp05/ex00/Bureaucrat.cpp
@@ -6,9 +6,19 @@
 /*   By: jmetayer <jmetayer@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/18 17:45:59 by jmetayer          #+#    #+#             */
-/*   Updated: 2026/08/18 17:46:20 by jmetayer         ###   ########.fr       */
+/*   Updated: 2026/08/18 17:51:07 by jmetayer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Bureaucrat.hpp"
 
+    Bureaucrat ( void );
+    Bureaucrat ( std::string name );
+    Bureaucrat& operator=( const Bureaucrat& copy );
+    Bureaucrat ( const Bureaucrat &copy );
+    ~Bureaucrat();
+
+    void setName( std::string name );
+    std::string getName( void );
+    void setRange( int range );
+    int getRange ( void );
