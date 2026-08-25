25c57e566170d7652d81a1422e96632bc79d40cb
Julien <user@MacBook-Air-de-Julien.local>
2026-08-25 16:18:05 +0200

    SCF ex02

---
+++ cpp05/ex02/ShrubberyCreationForm.cpp
@@ -0,0 +1,31 @@
+#include "ShrubberyCreationForm.hpp"
+
+ShruberryCreationForm( void ) : _name("Default_SCF"), 
+{
+
+}
+ShruberryCreationForm& operator=( const ShruberryCreationForm& copy)
+{
+
+}
+ShruberryCreationForm( const ShruberryCreationForm& copy)
+{
+
+}
+~ShruberryCreationForm( void )
+{
+
+}
+ShruberryCreationForm( std::string _copy )
+{
+
+}
+void	execute(void) const
+{
+
+}
+
+std::string const&	getTarget(void) const
+{
+
+}
--- cpp05/ex02/ShrubberyCreationForm.hpp
+++ cpp05/ex02/ShrubberyCreationForm.hpp
@@ -1,11 +1,27 @@
 #ifndef SHRUBBERYCREATIONFORM_HPP
 #define SHRUBBERYCREATIONFORM_HPP
 
+#include "AForm.hpp"
+#include <iostream>
+
 class ShruberryCreationForm : virtual public AForm 
 {
+    private :
+    std::string _target;
 
-}
+    public :
 
+    //Orthodox Canonical Form + constructors
+    ShruberryCreationForm( void );
+    ShruberryCreationForm& operator=( const ShruberryCreationForm& copy);
+    ShruberryCreationForm( const ShruberryCreationForm& copy);
+    ~ShruberryCreationForm( void );
 
+    ShruberryCreationForm( std::string _copy );
+    void	execute(void) const;
+    //getter
+    std::string const&	getTarget(void) const;
+
+};
 
 #endif
