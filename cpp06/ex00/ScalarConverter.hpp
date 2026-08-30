2e85491c7da8d758b748df3e22f7f8c27ccf5912
Julien <user@macbook-air-de-julien.home>
2026-08-30 17:55:58 +0200

    CPP06 ex00 beginning

---
+++ cpp06/ex00/ScalarConverter.hpp
@@ -0,0 +1,21 @@
+#ifndef SCALARCONVERTER_HPP
+#define SCALARCONVERTER_HPP
+
+#include <iostream>
+
+class ScalarConverter {
+    private :
+    
+    
+    public :
+    ScalarConverter( void );
+    ScalarConverter& operator=( const ScalarConverter& copy );
+    ScalarConverter( const ScalarConverter& copy );
+    ~ScalarConverter( void );
+
+
+
+}
+
+
+#endif
