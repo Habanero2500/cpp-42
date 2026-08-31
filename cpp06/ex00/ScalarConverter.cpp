02270b2f34c475e336b574b559d8fc6862bd58d2
jmetayer <julien.metayer98@gmail.com>
2026-08-31 18:25:45 +0200

    Commit 31082026

--- cpp06/ex00/ScalarConverter.cpp
+++ cpp06/ex00/ScalarConverter.cpp
@@ -1,1 +1,57 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ScalarConverter.cpp                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jmetayer <jmetayer@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/08/31 16:59:47 by jmetayer          #+#    #+#             */
+/*   Updated: 2026/08/31 18:15:26 by jmetayer         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "ScalarConverter.hpp"
+
+bool isChar(const std::string &str)
+{
+    if(str.length() == 1 && isalpha(str[0]))
+        return true;
+    return false;
+}
+bool isInt (const std::string &str)
+{
+    int i(0);
+    if(str[i] == '-' || str[i] == '+')
+        i++;
+    while(str[i])
+    {
+        if( str[i] < '0' || str[i] > '9')
+            return false;
+    }
+    return true;
+}
+
+int main( void )
+{
+    std::string str;
+    std::cout << "Quel test ? " << std::endl;
+    std::cin >> str;
+    std::cout << "Is char : " << isChar(str) << std::endl;
+    std::cout << "Is int : " << isChar(str) << std::endl;
+
+    
+}
+
+// bool isFloat(const std::string &str)
+// {
+    
+// }
+// bool isDouble(const std::string &str)
+// {
+    
+// }
+
+// static void convert( std::string str )
+// {
+    
+// }
--- cpp06/ex00/ScalarConverter.hpp
+++ cpp06/ex00/ScalarConverter.hpp
@@ -1,21 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ScalarConverter.hpp                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jmetayer <jmetayer@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/08/31 16:59:53 by jmetayer          #+#    #+#             */
+/*   Updated: 2026/08/31 18:18:18 by jmetayer         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef SCALARCONVERTER_HPP
 #define SCALARCONVERTER_HPP
 
 #include <iostream>
+#include <string>
+#include <climits>
 
 class ScalarConverter {
+    
     private :
-    
-    
-    public :
     ScalarConverter( void );
     ScalarConverter& operator=( const ScalarConverter& copy );
     ScalarConverter( const ScalarConverter& copy );
     ~ScalarConverter( void );
+    
+    public :
+    static void convert( std::string str );
 
+};
 
-
-}
+bool isChar(const std::string &str);
+bool isInt (const std::string &str);
 
 
 #endif
