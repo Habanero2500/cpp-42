f5bbcb0d25f14aa4306ae5694139877b55ee3f5b
jmetayer <julien.metayer98@gmail.com>
2026-09-01 17:03:56 +0200

    Commit fix of the day

--- cpp06/ex00/ScalarConverter.cpp
+++ cpp06/ex00/ScalarConverter.cpp
@@ -6,7 +6,7 @@
 /*   By: jmetayer <jmetayer@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/31 16:59:47 by jmetayer          #+#    #+#             */
-/*   Updated: 2026/09/01 13:47:22 by jmetayer         ###   ########.fr       */
+/*   Updated: 2026/09/01 16:01:01 by jmetayer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ static bool isChar(const std::string &str)
 }
 static bool isInt ( const std::string &str )
 {
-    double long res(1);
+    long long res(1);
     unsigned int i(0);
     if(str[i] == '-' || str[i] == '+')
     {
@@ -41,14 +41,52 @@ static bool isInt ( const std::string &str )
 }
 
 
-// bool isFloat(const std::string &str)
-// {
+// Double jusqu'a 16 nombres apres la virgule contre 7 pour le float. Float finit par un f 
+
+static bool isDouble(const std::string &str)
+{
+    unsigned int i(0);
+    bool point = false;
     
-// }
-// bool isDouble(const std::string &str)
-// {
-    
-// }
+
+
+    i = 0;
+    while(i < str.length())
+    {
+        if( str[i] < '0' || str[i] > '9' || str[i] == '.')
+            return false;
+        if(str[i] != 0 && str[i] == '.' && point == false)
+            point = true;
+        if(str[i] != 0 && str[i] == '.' && point == true)
+            return false;
+        i++;
+    }
+    if(point == true)
+        return true;
+    return false;
+}
+
+bool isDouble(const std::string &str)
+{
+    long long res(1);
+    unsigned int i(0);
+    if(str[0] == '-' || str[0] == '+')
+    {
+        if(str[0] == '-')
+            res *= -1;
+        i++;
+    }
+    while(i < str.length())
+    {
+        if( str[i] < '0' || str[i] > '9')
+            return false;
+        res = res * 10 + (str[i] - '0');
+        i++;
+    }
+    if(res <= INT_MAX && res >= INT_MIN)
+        return true;
+    return false;
+}
 
 static e_type whichType( const std::string &str )
 {
@@ -56,10 +94,9 @@ static e_type whichType( const std::string &str )
         return INT;
     else if( isChar(str) == true)
         return CHAR;
-    // else if( isDouble(str) == true)
-    //     return DOUBLE;
-    // else if( isFloat (str) == true )
-    //     return FLOAT;
+    else if( isDouble(str) == true)
+        return DOUBLE;
+
     else
         return NONE;
 }
@@ -68,5 +105,11 @@ static e_type whichType( const std::string &str )
 void ScalarConverter::convert( const std::string &str )
 {
     e_type type = whichType(str);
-    std::cout << "Type : " << type << std::endl; 
+    if(type == INT)
+        std::cout << "Type : INT" << std::endl; 
+    else if(type == CHAR)
+        std::cout << "Type : CHAR" << std::endl;
+    else
+        std::cout << "Type : NONE" << std::endl;
+
 }
